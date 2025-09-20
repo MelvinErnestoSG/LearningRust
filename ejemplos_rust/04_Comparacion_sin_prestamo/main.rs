48a4843af017def93b5dae4781a503fba8d22781
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2025-09-20 14:25:41 -0500

    Add Comparacion_sin_prestamo

--- ejemplos_rust/04_Comparacion_sin_prestamo/main.rs
+++ ejemplos_rust/04_Comparacion_sin_prestamo/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    //(transferencia de propiedad).
-    let mensaje1 = String::from("Sin préstamo.");
+    // Transferencia de propiedad.
+    let mensaje1 = String::from("Sin usar el préstamo.");
     let mensaje2 = mensaje1; // mensaje1 ya no es válido después de esta línea.
     // println!("{}", mensaje1); // Esto causaría un error de compilación.
     println!("{}", mensaje2); // Solo mensaje2 es válido.
